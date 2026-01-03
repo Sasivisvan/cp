227dc17558b0379ee368d868e0d6e8ea59d30ba6
sasi visvan <sasivisvan@gmail.com>
2026-01-03 22:02:39 +0530

    I did question no 605 on leetcode

---
+++ 605.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        if (1==flowerbed.size()){
+            if(n==0 && flowerbed[0]==1)return true;
+            if(n==1 && flowerbed[0]==0)return true;
+            
+        } 
+        
+        for (int i = 0; i < flowerbed.size(); i++) {
+            if (count == n) {
+                return true;
+                // cout<<i<<" ";
+            }
+            if (i == 0 && i!=flowerbed.size()-1 && flowerbed[i] == 0 && flowerbed[i + 1] == 0){
+                flowerbed[i]=1;
+                count++;
+                // cout<<i<<" ";
+            }
+            else if (i == flowerbed.size()-1 && flowerbed[i] == 0 && flowerbed[i - 1] == 0){
+                flowerbed[i]=1;
+                count++;
+                // cout<<i<<" ";
+            }
+            else if (i!=0 && i!=flowerbed.size()-1 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0 &&
+                flowerbed[i] == 0) {
+                flowerbed[i] = 1;
+                // cout<<i<<" ";
+                count++;
+            }
+            if (count == n) {
+                return true;
+                // cout<<i<<" ";
+            }
+            
+        }
+        if (count == n) {
+                return true;
+                // cout<<i<<" ";
+            }
+        
+        return false;
+    }
+};
