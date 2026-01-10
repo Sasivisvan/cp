4db0e8e88b9ef64bf8899fe5980250a46075e34b
sasi visvan <sasivisvan@gmail.com>
2026-01-10 17:06:45 +0530

    Solved question 2264 on leetcode easy

---
+++ 2264.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string ans = "";
+        for (int i = 0; i < num.size() - 2; i++) {
+            if (num[i] == num[i + 1] && num[i + 1] == num[i + 2] ) {
+                if (ans.size() == 0) {
+                    ans = num.substr(i, 3);
+                } else if (ans[0] < num[i]) {
+                    ans = num.substr(i, 3);
+                }
+            }
+        }
+        return ans;
+    }
+};
